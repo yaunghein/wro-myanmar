b64e2382f6a08969aab071b2046a1751ab06f700
Yan Aung Hein <yanaunghein311@gmail.com>
2024-05-12 20:53:04 +0630

    new layout for partner page

--- app/src/lib/components/partners/Card.svelte
+++ app/src/lib/components/partners/Card.svelte
@@ -12,25 +12,44 @@
 		silver: 'bg-off-white-dark text-white',
 		school: 'bg-gradient-primary text-white'
 	};
+
+	const sponsorTextThemes: Record<string, Record<string, string>> = {
+		school: { color: 'bg-gradient-primary text-transparent bg-clip-text' },
+		premium: { color: 'text-accent-three' },
+		gold: { color: 'bg-gradient-robomission text-transparent bg-clip-text' },
+		silver: { color: 'text-off-white-dark' }
+	};
 </script>
 
-<div class="flex w-full shrink-0 flex-col items-start gap-4 sm:w-96">
-	<h3 class="font-black text-[2rem] leading-none tracking-tight sm:text-5xl">
-		{cleanText(partner.name)}
-	</h3>
-	<div class="overflow-hidden">
+<div class="flex h-full w-full shrink-0 flex-col items-start gap-6">
+	<div
+		class="font-black text-sm uppercase leading-none tracking-tight sm:text-base {sponsorTextThemes[
+			partner.type
+		].color}"
+	>
+		Venue sponsor
+	</div>
+
+	<div class="-mb-3 -mt-[1.1rem] h-[1px] w-full shrink-0 sm:-mt-[1.35rem] {themes[partner.type]}" />
+
+	<div class="aspect-[1.33/1] w-full overflow-hidden bg-off-white">
 		<img
 			src={urlFor(partner.image.image).url()}
 			alt={cleanText(partner.image.caption)}
-			class="aspect-[1.33/1] w-full object-cover"
+			class="h-full w-full object-cover"
 		/>
 	</div>
-	<p class="leading-tight">{cleanText(partner.description)}</p>
+	<h3 class="font-black text-2xl uppercase leading-none tracking-tight sm:text-[1.75rem]">
+		{cleanText(partner.name)}
+	</h3>
+	<p class="mb-3 text-sm leading-tight sm:text-base">{cleanText(partner.description)}</p>
 	<a
 		target="_black"
 		href={cleanText(partner.link)}
-		class="mt-3 px-6 py-4 font-black uppercase leading-none tracking-tight {themes[partner.type]}"
+		class="mt-auto w-full px-6 py-4 text-center font-black text-sm uppercase leading-none tracking-tight sm:text-base {themes[
+			partner.type
+		]}"
 	>
-		{cleanText(partner.label)}
+		Visit Website
 	</a>
 </div>
